0612e46315b699ee1e5ca61e4fe5bb04ec72c3ee
husainharmoni <husainahmad@gmail.com>
2025-02-12 16:28:54 +0700

    [AUTH] Pooling connection

--- src/main/java/com/harmoni/auth/AuthApplication.java
+++ src/main/java/com/harmoni/auth/AuthApplication.java
@@ -3,7 +3,6 @@ package com.harmoni.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
@@ -11,7 +10,7 @@ import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServic
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class,
 		UserDetailsServiceAutoConfiguration.class})
 @EnableAutoConfiguration(
-		exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+		exclude = {HibernateJpaAutoConfiguration.class})
 public class AuthApplication {
 
 	public static void main(String[] args) {
